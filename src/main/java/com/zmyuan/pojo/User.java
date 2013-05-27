dbc40ab70674e972e5fc6d6ace69951a63f55b60
zhudebin <zhudebin@zhudebin-PC>
2013-05-28 00:14:31 +0800

    初始创建，架构搭建

---
+++ src/main/java/com/zmyuan/pojo/User.java
@@ -0,0 +1,44 @@
+package com.zmyuan.pojo;
+
+import java.util.Date;
+
+public class User {
+
+	private int id;
+	private String username;
+	private String password;
+	private Date crtTime;
+	private Date updTime;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Date getCrtTime() {
+		return crtTime;
+	}
+	public void setCrtTime(Date crtTime) {
+		this.crtTime = crtTime;
+	}
+	public Date getUpdTime() {
+		return updTime;
+	}
+	public void setUpdTime(Date updTime) {
+		this.updTime = updTime;
+	}
+	
+}
